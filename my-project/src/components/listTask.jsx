7c7b620cf46abdc4584320f1f7f002776dacb18a
Adil <adilc0070@gmail.com>
2024-02-11 14:02:58 +0530

    finished

--- my-project/src/components/listTask.jsx
+++ my-project/src/components/listTask.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import { FaTrash,FaCheckSquare } from 'react-icons/fa';
+import { FaTrash, FaCheckSquare } from 'react-icons/fa';
 
-function ListTask({task,index,removeTask}) {
+function ListTask({ task, index, removeTask, completeTask, notCompleteTask }) {
   return (
     <>
       <div className='flex items-center justify-between py-2 border-b'>
-      <div className="flex-grow md:w-1/2">
-        <div className="text-gray-800 overflow-hidden whitespace-nowrap overflow-ellipsis">{task.title}</div>
+        <div className="flex-grow md:w-1/2">
+          <div className="text-gray-800 overflow-hidden whitespace-nowrap overflow-ellipsis">{task.title}</div>
+        </div>
+        <div className="flex items-center ml-3">
+          {!task.checked ? (
+            <button className='bg-green-700 hover:bg-green-900 text-white py-2 px-4 rounded mr-2' onClick={() => { completeTask(index) }}>
+              <FaCheckSquare />
+            </button>
+          ) : (
+            <button className='bg-gray-400 hover:bg-blue-600 text-white py-2 px-4 rounded mr-2' onClick={() => { notCompleteTask(index) }}>
+              <FaCheckSquare />
+            </button>
+          )}
+          <button className='bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded' onClick={() => { removeTask(index) }}>
+            <FaTrash />
+          </button>
+        </div>
       </div>
-      <div className="flex items-center ml-3">
-        <button className='bg-green-700 hover:bg-green-900 text-white py-2 px-4 rounded mr-2'>
-          <FaCheckSquare/>
-        </button>
-        <button className='bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded' onClick={()=>{removeTask(index)}}>
-        <FaTrash />
-        </button>
-      </div>
-    </div>
     </>
-    
   );
 }
 
--- my-project/src/components/toDo.jsx
+++ my-project/src/components/toDo.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import alertify from 'alertifyjs';
 
 import AddTask from './addTask.jsx';
@@ -6,13 +6,20 @@ import ListTask from './listTask.jsx';
 
 function ToDo() {
   const [tasks, setTasks] = useState([]);
-  let check=(v,arr)=>{
-    let re=[]
-      for(let i=0;i<arr.length;i++){
-        re.push(arr[i].title==v)
-      }
-      return re.includes(true)
-  }
+  useEffect(()=>{
+    let storedTasks=localStorage.getItem('tasks');
+    if (storedTasks) {
+      setTasks(JSON.parse(storedTasks));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+  let check = (v, arr) => {
+    return arr.some(task => task.title === v);
+  };
   let addTask=(title)=>{
     if(title.trim()==''){
       alertify.set('notifier','position', 'top-center');
@@ -22,7 +29,9 @@ function ToDo() {
       alertify.set('notifier','position', 'top-center');
       alertify.warning('Task Already Exist');
     }else{
-      const newTask=[...tasks,{title}]
+      const newTask=[...tasks,{title,checked:false}]
+      alertify.set('notifier','position', 'top-right');
+      alertify.success('Task Added');
       setTasks(newTask);
     }
   }
@@ -30,6 +39,27 @@ function ToDo() {
     const newTask=[...tasks]
     newTask.splice(index,1);
     setTasks(newTask)
+  }
+  let completeTask=(index)=>{
+    let completedTask=tasks.map((task,i)=>{
+      if(i===index){
+        alertify.success('Task completed');
+        return {...task,checked:true}
+      }else{
+        return task
+      }
+    });
+    setTasks(completedTask)
+  }
+  let notCompleteTask=(index)=>{
+    let notCompletedTask=tasks.map((task,i)=>{
+      if(i===index){
+        return {...task,checked:false}
+      }else{
+        return task
+      }
+    });
+    setTasks(notCompletedTask)
   }
   return (
     <div className='flex justify-center items-center min-h-screen bg-slate-900'>
@@ -40,7 +70,7 @@ function ToDo() {
         </div>
         <div className="overflow-y-auto max-h-80">
             {tasks.map((task, index) => (
-              <ListTask key={index} task={task} removeTask={removeTask} index={index} />
+              <ListTask key={index} task={task} removeTask={removeTask} index={index} completeTask={completeTask} notCompleteTask={notCompleteTask}/>
             ))}
         </div>
       </div>
